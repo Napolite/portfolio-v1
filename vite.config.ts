bb18579e53cefe4f5f2b6765d3b5484fbfc6d78a
Constantine <Okonemmanuelmma@gmail.com>
2026-06-22 12:05:55 +0100

    configure service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.mode === "navigate",
+            handler: "StaleWhileRevalidate",
+
+            options: {
+              cacheName: "html-cache",
+              expiration: {
+                maxEntries: 1,
+              },
+            },
+          },
+        ],
+      },
+    }),
   ],
-})
+});
